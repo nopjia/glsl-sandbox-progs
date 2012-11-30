9d5f8d29ee87cd30fd650cf5e726f391f69528cf
Nop Jiarathanakul <prutsdomj@gmail.com>
2012-11-30 03:37:04 -0500

    reindent and organize

--- game-of-life.glsl
+++ game-of-life.glsl
@@ -44,18 +44,16 @@ bool isLive;
 //---------------------------------------------------------
 
 float circle (vec2 center, float radius) {
-    float dist = distance(center, pos);
-    
-    return dist < radius ? 1.0 : 0.0;
+  return distance(center, pos) < radius ? 1.0 : 0.0;
 }
 
 float rect (vec2 center, vec2 b) {
-    vec2 bMin = center-b;
-    vec2 bMax = center+b;
-    return ( 
-        pos.x > bMin.x && pos.y > bMin.y && pos.x < bMax.x && pos.y < bMax.y 
-        ) ? 
-        1.0 : 0.0;
+  vec2 bMin = center-b;
+  vec2 bMax = center+b;
+  return ( 
+    pos.x > bMin.x && pos.y > bMin.y && 
+    pos.x < bMax.x && pos.y < bMax.y ) ? 
+    1.0 : 0.0;
 }
 
 //---------------------------------------------------------
@@ -63,45 +61,45 @@ float rect (vec2 center, vec2 b) {
 //---------------------------------------------------------
 
 int countNeighbors(vec2 p) {
-    int count = 0;
-    
-    #define KERNEL_R 1
-    for (int y=-KERNEL_R; y<=KERNEL_R; ++y)
-    for (int x=-KERNEL_R; x<=KERNEL_R; ++x) {
-        vec2 spoint = uvUnit*vec2(float(x),float(y));
-        if ( texture2D(backbuffer, uv+spoint).a > 0.0 )
-            ++count;
-    }
-    
-    if (isLive)
-        --count;
-    
-    return count;
+  int count = 0;
+  
+  #define KERNEL_R 1
+  for (int y=-KERNEL_R; y<=KERNEL_R; ++y)
+  for (int x=-KERNEL_R; x<=KERNEL_R; ++x) {
+    vec2 spoint = uvUnit*vec2(float(x),float(y));
+    if ( texture2D(backbuffer, uv+spoint).a > 0.0 )
+      ++count;
+  }
+  
+  if (isLive)
+    --count;
+  
+  return count;
 }
 
 float gameStep() {
-    isLive = texture2D(backbuffer, uv).a > 0.0;
-    int neighbors = countNeighbors(uv);
-    
-    if (isLive) {
-        if (neighbors < 2)
-            return 0.0;
-        else if (neighbors > 3)
-            return 0.0;
-        else 
-            return 1.0;
-    }
-    else {
-        if (neighbors==3)
-            return 1.0;
-        else
-            return 0.0;
-    }
+  isLive = texture2D(backbuffer, uv).a > 0.0;
+  int neighbors = countNeighbors(uv);
+  
+  if (isLive) {
+    if (neighbors < 2)
+      return 0.0;
+    else if (neighbors > 3)
+      return 0.0;
+    else 
+      return 1.0;
+  }
+  else {
+    if (neighbors==3)
+      return 1.0;
+    else
+      return 0.0;
+  }
 }
 
 vec3 ghosting() {
-    #define DECAY 0.90
-    return DECAY * texture2D(backbuffer, uv).rgb;
+  #define DECAY 0.90
+  return DECAY * texture2D(backbuffer, uv).rgb;
 }
 
 //---------------------------------------------------------
@@ -109,37 +107,37 @@ vec3 ghosting() {
 //---------------------------------------------------------
 
 void main( void ) {
-    aspect = resolution.x/resolution.y;
-    uvUnit = 1.0 / resolution.xy;
-    
-    uv = ( gl_FragCoord.xy / resolution.xy );
-    pos = (uv-0.5);
-    pos.x *= aspect;
-    
-    pmouse = mouse-vec2(0.5);
-    pmouse.x *= aspect; 
-    
-    
-    
-    float live = 0.0;
-    
-    // seed shape
-    float radius = 0.025;    
-    live += circle(pmouse, radius);
-    
-    // sim game
-    live += gameStep();
-    
-    
-    
-    // draw out
-    vec3 color = vec3(0.9, 0.0, 0.8);
-    color *= live;
-    color += ghosting();
-    vec4 cout = vec4(color, live);
-    
-    // clear;
-    //cout = vec4(0.0);
-	
-    gl_FragColor = cout;
+  aspect = resolution.x/resolution.y;
+  uvUnit = 1.0 / resolution.xy;
+  
+  uv = ( gl_FragCoord.xy / resolution.xy );
+  pos = (uv-0.5);
+  pos.x *= aspect;
+  
+  pmouse = mouse-vec2(0.5);
+  pmouse.x *= aspect; 
+  
+  
+  
+  float live = 0.0;
+  
+  // seed shape
+  float radius = 0.025;    
+  live += circle(pmouse, radius);
+  
+  // sim game
+  live += gameStep();
+  
+  
+  
+  // draw out
+  vec3 color = vec3(0.9, 0.0, 0.8);
+  color *= live;
+  color += ghosting();
+  vec4 cout = vec4(color, live);
+  
+  // clear;
+  //cout = vec4(0.0);
+
+  gl_FragColor = cout;
 }
